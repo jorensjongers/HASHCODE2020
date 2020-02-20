83f729f5629d08e0f8f1e0b0c45a06b3a53b0c0c
lottever <lotte.verheyden@gmail.com>
2020-02-20 20:16:06 +0100

    sorteren van boeken op scores functie in main

--- main.py
+++ main.py
@@ -12,3 +12,13 @@ input = parser.parse_input(f)
 
 libs = [{'lib_id' : 0, 'nb_books' : 2, 'books' : [2, 3]}, {'lib_id' : 1, 'nb_books' : 3, 'books' : [0,1, 3]}]
 parser.output(libs, OUTPUT_FILE)
+
+#scores = alles scores van alle boeken
+def sort_book_ids(books, scores):
+    scores_for_lib = []
+    for book in books:
+        scores_for_lib.append(scores[book])
+    return [x for y,x in sorted(zip(scores_for_lib, books))]
+
+
+    

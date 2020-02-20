1088cc128004a910410c241704f3bba12fbc49ef
Kemal Mahmuljin <kemal.mahmuljin@student.kuleuven.be>
2020-02-20 20:42:43 +0100

    serial

--- Serial_Approach.py
+++ Serial_Approach.py
@@ -1,3 +1,5 @@
+from parser import *
+
 def LibScore(daysForScanning, signupForLib, bookScores, nbBookinLib, booksPerDay):
     count = daysForScanning - signupForLib
     score = 0
@@ -5,10 +7,30 @@ def LibScore(daysForScanning, signupForLib, bookScores, nbBookinLib, booksPerDay
     while ((count >0) and  (nbBookinLib>0)):       
         count -= 1    
         for book in range(min(booksPerDay, nbBookinLib)):
-            print(book)
+            #print(book)
             score += bookScores[nbBookinLib -1]
             nbBookinLib -= 1
     return score
+
+
+def optimalLibrary(daysForScanning, lstLibrary, AllBookScores):
+    optScore= 0 
+    optLib = None
+    for lib in lstLibrary:
+        bookscore = sort_book_ids(lib.get("books"), AllBookScores)[1]
+        currentScore = libScore(daysforScanning, lib.get("signup_time"), bookscores, lib.get("nb_books"), lib.get("books_per_day"))
+        if (currentScore > optScore): ## opt
+            optScore = currentScore
+            optLib = lib
+    return optLib
+
+def libraryOrder(): 
+    deadline = get_deadline()
+
+    while (deadline > 0) : 
+        opt_lib = optimalLibrary(deadline, get_libraries())
+
+    return 
 
 
 #test1

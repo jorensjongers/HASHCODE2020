4f099826dca6a73124b09157d9a5d49624b1262c
lottever <lotte.verheyden@gmail.com>
2020-02-20 19:27:12 +0100

    parser af

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import parser
 
-FILE_NAME = "a_example.in"
-OUTPUT_FILE = "submission_landscape.txt"
-
-
-
+FILE_NAME = "a_example.txt"
+OUTPUT_FILE = "submission.txt"
 
 f = open(FILE_NAME, "r")
-out = parser.parse_input(f)
-print(out)
+input = parser.parse_input(f)
+
+
+
+
+
+libs = parser.get_libraries(input)
+print(libs)
--- parser.py
+++ parser.py
@@ -1,6 +1,43 @@
 def parse_input(file):
     list = [line.split(' ') for line in file.read().split('\n')]
+    print(list)
     return list
+
+def get_nb_books(list):
+    return int(list[0][0])
+
+def get_nb_libraries(list):
+    return int(list[0][1])
+
+def get_deadline(list):
+    return int(list[0][2])
+
+def get_scores(list):
+    result = []
+    for score in list[1]:
+        new_score = int(score)
+        result.append(new_score)
+    return result
+
+def get_libraries(lst):
+    result = []
+    for id in range(0, get_nb_libraries(lst) * 2, 2):
+        nb_books = int(lst[id+2][0])
+        signup_time = int(lst[id+2][1])
+        books_per_day = int(lst[id+2][2])
+        books = []
+        for book in lst[id+3]:
+            new_book = int(book)
+            books.append(new_book)
+        lib = {}
+        lib["nb_books"] = nb_books
+        lib["signup_time"]=signup_time
+        lib["books_per_day"]=books_per_day
+        lib["books"] = books
+        result.append(lib)
+    return result
+
+
 
 def output(output_file, slideshow):
     # TODO aanpassen aan opgave, niet zo gebruiken!!!

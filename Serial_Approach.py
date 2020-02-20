b8818972ac37cdd441082e5760376a960f9af7d2
Kemal Mahmuljin <kemal.mahmuljin@student.kuleuven.be>
2020-02-20 21:04:21 +0100

    liofsgrl

--- Serial_Approach.py
+++ Serial_Approach.py
@@ -3,34 +3,36 @@ from parser import *
 def LibScore(daysForScanning, signupForLib, bookScores, nbBookinLib, booksPerDay):
     count = daysForScanning - signupForLib
     score = 0
-
+    scannedbooks = 0
     while ((count >0) and  (nbBookinLib>0)):       
         count -= 1    
         for book in range(min(booksPerDay, nbBookinLib)):
             #print(book)
             score += bookScores[nbBookinLib -1]
             nbBookinLib -= 1
-    return score
-
+            scannedbooks +=1
+    return score, scannedbooks
 
 def optimalLibrary(daysForScanning, lstLibrary, AllBookScores):
     optScore= 0 
     optLib = None
     for lib in lstLibrary:
-        bookscore = sort_book_ids(lib.get("books"), AllBookScores)[1]
-        currentScore = libScore(daysforScanning, lib.get("signup_time"), bookscores, lib.get("nb_books"), lib.get("books_per_day"))
-        if (currentScore > optScore): ## opt
-            optScore = currentScore
+        bookscore = sort_book_ids(lib.get("books"), AllBookScores)
+        currentScore = libScore(daysforScanning, lib.get("signup_time"), bookscores[1], lib.get("nb_books"), lib.get("books_per_day"))
+        if (currentScore[0] > optScore): ## opt
+            optScore = currentScore[0]
             optLib = lib
-    return optLib
+            optOrderedBooks = bookscore[-1:books-1-currentScore[1]: -1] 
+    return optLib, optOrderedBooks
 
 def libraryOrder(): 
     deadline = get_deadline()
-
+    libArray = []
     while (deadline > 0) : 
         opt_lib = optimalLibrary(deadline, get_libraries())
-
-    return 
+        libArray.append(opt_lib)
+        deadline -= opt_lib.get("signup_time")
+    return libArray
 
 
 #test1
